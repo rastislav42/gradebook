b4962f90320a2612dc513b36eb978ce9f7546997
rastislav42 <rastislav42@outlook.com>
2023-03-31 15:56:24 -0700

    Create Book.cs

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+      class Book
+      {
+        
+        public void AddGrade(double grade)
+        {
+            pass
+        }
+        List<double> grades;
+     }
+}
