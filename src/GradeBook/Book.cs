460295d6bf2e832cd28563984f67205278bee1ff
rastislav42 <54588302+rastislav42@users.noreply.github.com>
2023-04-21 09:29:07 +0200

    sd

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -4,14 +4,35 @@ namespace GradeBook
 {
       class Book
       {
-        public Book()
+        public Book(string name)
         {
           grades = new List<double>();
+          this.name = name;
         }
         public void AddGrade(double grade)
         {
             grades.Add(grade);
         }
+
+       public void ShowStatistic()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var number in grades)
+            {
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is  {result:N1}");
+        }
+
         List<double> grades;
+        String name;
      }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,4 @@
-﻿sing System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -10,16 +10,12 @@ namespace GradeBook
             var book = new Book("Scott's Grade Book");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-            var grades = new List<double>() {12.7,10.3,6.11,4.1 };
-            grades.Add(56.1);
+            book.AddGrade(77.5);
+            book.ShowStatistic();
+            //var grades = new List<double>() {12.7,10.3,6.11,4.1 };
+            //grades.Add(56.1);
 
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is  {result:N1}");
+            
          }
     }
 }
